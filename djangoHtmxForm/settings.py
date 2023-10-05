66b62d60aa01cc8dfd7930acb1e324f65985a89e
enos lyznne <emuthiani26@gmail.com>
2023-10-06 01:21:40 +0300

    :tada:c

--- djangoHtmxForm/settings.py
+++ djangoHtmxForm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$=^qz3nm&3_x&*kh+obn#l638x^_yt*873j7d!2&m*sv0cogre'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
