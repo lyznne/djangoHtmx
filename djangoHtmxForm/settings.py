a477e6b3ea7021b2f764c11dced2aacb02f9abe2
enos <emuthiani26@gmail.com>
2024-03-20 00:24:23 +0300

    render dev

--- djangoHtmxForm/settings.py
+++ djangoHtmxForm/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=^qz3nm&3_x&*kh+obn#l638x^_yt*873j7d!2&m*sv0cogre'
+# SECRET_KEY = 'django-insecure-$=^qz3nm&3_x&*kh+obn#l638x^_yt*873j7d!2&m*sv0cogre'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -73,6 +79,8 @@ WSGI_APPLICATION = 'djangoHtmxForm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+database_url = os.environ.get("DATABASE_URL")
+
 
 DATABASES = {
     'default': {
@@ -80,7 +88,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# DATABASES = {
+#     'default': dj_database_url.parse(database_url)
+# }
+# DATABASES = ['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
